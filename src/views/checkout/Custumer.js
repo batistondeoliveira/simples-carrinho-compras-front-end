839ff58cc7bebbdbaef494db24a8f961cf0330a2
Eliel <eliel@avance.inf.br>
2021-05-20 15:35:20 -0300

    Separando a secao de clientes e produtos em componentes

---
+++ src/views/checkout/Custumer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Custumer() {
+    return (                    
+        <div class="checkout-grid-custumer">
+            <div class="custumer-grid-card">
+                <i class="fas fa-shopping-bag"></i>
+                <div class="custumer-detail">  
+                    <p class="custumer-detail-name">Armazem do seu Zé</p>
+                    <p class="custumer-detail-timeout">09:54:34 min restante</p>
+                </div>
+            </div>
+        </div>            
+    );
+}
--- src/views/checkout/index.js
+++ src/views/checkout/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { Header } from '../../components/header'
+import Custumer from './Custumer'
 import Products from './Products'
 
-export default function Showcase() {
+export default function Checkout() {
     return (
         <>
             <Header />   
 
-            <Products />                         
+            <div class="checkout-grid">
+                <h2>Finalizar Pedido</h2>
+
+                <Custumer />
+
+                <Products />
+
+                <div class="checkout-grid-button">                
+                    <div class="button-grid-card">
+                        <button class="btn btn-checkout">
+                            Finalizar compra
+                        </button>
+                    </div>
+                </div>
+            </div>                          
         </>
     )
 }
