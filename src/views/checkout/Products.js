5f10e37aab0f13494eb1e50669aac7d0a1394783
Eliel <eliel@avance.inf.br>
2021-05-20 14:55:46 -0300

    Criado componentes para o checkout

---
+++ src/views/checkout/Products.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function ProductList() {
+    return (            
+        <div class="checkout-grid">
+            <h2>Finalizar Pedido</h2>
+
+            <div class="custumer">
+                <div class="custumer-grid-card">
+                    <i class="fas fa-shopping-bag"></i>
+                    <div class="custumer-detail">  
+                        <p class="custumer-detail-name">Armazem do seu Zé</p>
+                        <p class="custumer-detail-timeout">09:54:34 min restante</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="checkout-grid-products">
+                <div class="checkout-grid-card">                 
+                    <div class="checkout-actions">  
+                        <button class="btn">-</button>
+                        <label class="product-amount">1</label>                
+                        <button class="btn">+</button>
+                    </div>
+
+                    <img src="img/coca-cola.png" alt="Produto 1" />  
+                    
+                    <p class="product-name">Nome do Produto</p>
+                    <p class="product-price">R$ 124,90</p>                                    
+                </div> 
+                
+                <div class="checkout-grid-card">                 
+                    <div class="checkout-actions">  
+                        <button class="btn">-</button>
+                        <label class="product-amount">1</label>                
+                        <button class="btn">+</button>
+                    </div>
+
+                    <img src="img/coca-cola.png" alt="Produto 2" />  
+
+                    <p class="product-name">Nome do Produto</p>
+                    <p class="product-price">R$ 124,90</p>                                    
+                </div>
+            </div>
+
+            <div class="checkout-grid-button">
+                <button class="btn btn-checkout">
+                    Finalizar compra
+                </button>
+            </div>
+        </div>  
+    );
+}
---
+++ src/views/checkout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from '../../components/header'
+import Products from './Products'
+
+export default function Showcase() {
+    return (
+        <>
+            <Header />   
+
+            <Products />                         
+        </>
+    )
+}
