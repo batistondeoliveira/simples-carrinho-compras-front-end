71dd9c275f7d18394a02cbaee1cda0f891e7071b
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2021-05-20 22:13:41 -0300

    Alterado o componente para renderizar os produtos

--- src/views/showcase/Products.js
+++ src/views/showcase/Products.js
@@ -1,41 +1,35 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { indexAction } from '../../store/actions/products.actions';
 
 export default function Products() {
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.productsReducer.products);
+
+    const [ isLoadMore, setLoadMore ] = React.useState(false);
+    const [ query, setQuery ] = React.useState({ 
+        page: 1        
+    }); 
+
+    React.useEffect(() => {
+        dispatch(indexAction(query, isLoadMore));    
+
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (    
         <div class="products-grid">
             <h2>Produtos</h2>
             <div class="products-grid-container">
-                <div class="products-grid-card">        
-                    <img src="img/coca-cola.png" alt="Produto 1" />
-                    <p class="category">Categoria</p>
-                    <h3 class="product-name">Nome do Produto</h3>
-                    <p class="product-price">R$ 124,90</p>                    
-                    <button class="btn">Adicionar ao Carrinho</button>
-                </div>  
-                
-                <div class="products-grid-card">        
-                    <img src="img/fanta-lata.png" alt="Produto 2" />
-                    <p class="category">Categoria</p>
-                    <h3 class="product-name">Nome do Produto</h3>
-                    <p class="product-price">R$ 124,90</p>                    
-                    <button class="btn">Adicionar ao Carrinho</button>
-                </div>   
-
-                <div class="products-grid-card">        
-                    <img src="img/brigadeiro.png" alt="Produto 3" />
-                    <p class="category">Categoria</p>
-                    <h3 class="product-name">Nome do Produto</h3>
-                    <p class="product-price">R$ 124,90</p>                    
-                    <button class="btn">Adicionar ao Carrinho</button>
-                </div>   
-
-                <div class="products-grid-card">        
-                    <img src="img/agua-mineral-sem-gas.png" alt="Produto 4" />
-                    <p class="category">Categoria</p>
-                    <h3 class="product-name">Nome do Produto</h3>
-                    <p class="product-price">R$ 124,90</p>                    
-                    <button class="btn">Adicionar ao Carrinho</button>
-                </div>   
+                {products.data.map((item, index) => (
+                    <div key={index} class="products-grid-card">        
+                        <img src="img/coca-cola.png" alt="Produto 1" />
+                        <p class="category">Categoria</p>
+                        <h3 class="product-name">{item.name}</h3>
+                        <p class="product-price">{item.price}</p> 
+                        <button class="btn">Adicionar ao Carrinho</button>
+                    </div>  
+                ))}                                                                            
             </div>
         </div>  
     );
