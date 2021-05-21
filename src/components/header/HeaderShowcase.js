16d9d26e844ed0edf1ebecaef862d07ed9646230
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2021-05-20 22:07:37 -0300

    Alterado componente para renderizar as categorias dentro do select

--- src/components/header/HeaderShowcase.js
+++ src/components/header/HeaderShowcase.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Header from './Header'
+import { indexAction } from '../../store/actions/category.actions';
+import { useDispatch, useSelector } from 'react-redux'
 
 export default function HeaderShowcase() {
+    const dispatch = useDispatch();
+    const categories = useSelector(state => state.categoryReducer.categories);    
+
+    const getProductsByCategory = (id) => {
+        alert(id);
+    }
+
+    React.useEffect(() => {
+        dispatch(indexAction());    
+
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    
     return (
         <Header>
             <form action="" id="search-form">
-                <select id="search" placeholder="Busque aqui">
-                    <option value="">
+                <select id="search" placeholder="Busque aqui" onChange={(event) => getProductsByCategory(event.target.value)}>
+                    <option value="-1">
                         Busque por categoria
                     </option>
-
-                    <option value="Produto 1">
-                        Produto 1
-                    </option>
+                    
+                    {categories.map((item, index) => (
+                        <option key={index} value={item.id}>
+                            {item.name}
+                        </option>
+                    ))}                                      
                 </select>                
             </form>
 
