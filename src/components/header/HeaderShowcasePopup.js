2089a04540e21ade5242f517cbe0a1c74bbcacc3
Eliel Batiston de Oliveira <batistondeoliveira@yahoo.com.br>
2021-05-23 23:15:53 -0300

    Alterado o componente para preencher a lista de produtos no carrinho de compras

--- src/components/header/HeaderShowcasePopup.js
+++ src/components/header/HeaderShowcasePopup.js
@@ -1,33 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { currency } from '../currency'
 
 export default function HeaderShowcasePopup() {
+    const shopping = useSelector(state => state.shoppingReducer.shopping)
+
     return (                    
-        <div class="shopping arrow-top-right popup">                                                 
-            <span class="shopping-popup-title">minha cesta</span>
+        <div class="shopping arrow-top-right popup">   
+            {shopping.uuid === undefined &&
+                <span class="shopping-empty">sua cesta está vazia</span>
+            }
 
-            <ul class="shopping-list">
-                <li class="shopping-item">                            
-                    <img src="img/coca-cola.png" class="image" alt="Produto 1" />
-                    <span class="product-name">Produto 1 XXXX  XXXXXXXXA</span>                                
-                    <span class="product-quant">Quantidade: 1</span>
-                    <span class="product-price">R$ 1.299,00</span>                            
-                </li>
+            {
+                <>                              
+                    <span class="shopping-popup-title">Minha cesta</span>
 
-                <li class="shopping-item">                            
-                    <img src="img/coca-cola.png" class="image" alt="Produto 1" />
-                    <span class="product-name">Produto 1 XXXX  XXXXXXXXA</span>                                
-                    <span class="product-quant">Quantidade: 1</span>
-                    <span class="product-price">R$ 1.299,00</span>                            
-                </li>
-            </ul>
+                    <ul class="shopping-list">
+                        {shopping.items.map((items, index) => {
+                            <li class="shopping-item">                            
+                                <img src={items.image} class="image" alt={items.name} />
+                                <span class="product-name">{items.name}</span>                                
+                                <span class="product-quant">Quantidade: {items.quant}</span>
+                                <span class="product-price">{currency(items.price * items.quant)}</span>
+                            </li>
+                        })}                        
+                    </ul>
 
-            <div class="shopping-bottom">
-                <span class="total">
-                    Total: <strong>R$ 1.299,00</strong>
-                </span>
-                
-                <button class="btn">ver minha cesta</button>
-            </div>                        
+                    <div class="shopping-bottom">
+                        <span class="total">
+                            Total: <strong>{ currency(shopping.amount) }</strong>
+                        </span>
+                        
+                        <Link class="btn" to="/checkout">
+                            ver minha cesta
+                        </Link>
+                    </div> 
+                </>
+            }                       
         </div>             
     );
 }
