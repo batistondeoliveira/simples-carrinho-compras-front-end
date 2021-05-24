8d6ca4396a28ad00dcdc1e9500cb450e99d4faa5
Eliel Batiston de Oliveira <batistondeoliveira@yahoo.com.br>
2021-05-23 23:08:45 -0300

    Alterado o componente para exibir a imagem do produto do objeto mockado

--- src/views/showcase/Products.js
+++ src/views/showcase/Products.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { indexAction } from '../../store/actions/products.actions';
 import { currency } from '../../components/currency'
+import { storeAction as shoppingStoreAction } from '../../store/actions/shopping.actions'
+import { uuid } from 'uuidv4'
 
 export default function Products() {
     const dispatch = useDispatch();
     const products = useSelector(state => state.productsReducer.products);
+    const shopping = useSelector(state => state.shoppingReducer.shopping);
 
     const [ isLoadMore, setLoadMore ] = React.useState(false);
     const [ query, setQuery ] = React.useState({ 
@@ -18,17 +21,32 @@ export default function Products() {
         //eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const addItem = (product) => {        
+        if (shopping.uuid === undefined) {
+            shopping.uuid = uuid(); 
+            shopping.custumer = 'Armazem do Zé';
+        }        
+
+        let item = {};
+
+        item.quant = 1;
+        item.products = product;
+        shopping.items.push(item);
+        shopping.amount += (item.quant * product);
+        dispatch(shoppingStoreAction(shopping));
+    }
+
     return (    
         <div class="products-grid">
             <h2>Produtos</h2>
             <div class="products-grid-container">
-                {products.data.map((item, index) => (
+                {products.data.map((product, index) => (
                     <div key={index} class="products-grid-card">        
-                        <img src="img/coca-cola.png" alt="Produto 1" />
+                        <img src={product.image} alt={product.name} />
                         <p class="category">Categoria</p>
-                        <h3 class="product-name">{item.name}</h3>
-                        <p class="product-price">{currency(item.price)}</p> 
-                        <button class="btn">Adicionar ao Carrinho</button>
+                        <h3 class="product-name">{product.name}</h3>
+                        <p class="product-price">{currency(product.price)}</p> 
+                        <button class="btn" onClick={() => addItem(product)}>Adicionar ao Carrinho</button>
                     </div>  
                 ))}                                                                            
             </div>
