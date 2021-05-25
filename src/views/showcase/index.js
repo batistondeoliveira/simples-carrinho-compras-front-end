994f2a31b5b06972ec1f69e6ca38b0567322a283
Eliel Batiston de Oliveira <batistondeoliveira@yahoo.com.br>
2021-05-24 22:57:50 -0300

    Enviado a pasta components para dentro da pasta view

--- src/views/showcase/index.js
+++ src/views/showcase/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { HeaderShowcase } from '../../components/header'
+import { HeaderShowcase } from '../components/header'
 import Products from './Products'
-import Footer from '../../components/footer'
+import Footer from '../components/footer'
 
 export default function Showcase() {
     return (
