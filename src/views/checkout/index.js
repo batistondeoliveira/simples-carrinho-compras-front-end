e4fa8696f42d631b97526ec55234912c7a929809
Eliel Batiston de Oliveira <batistondeoliveira@yahoo.com.br>
2021-05-24 23:22:52 -0300

    Alterado o componente para exibir a mensagem que o carrinho está vazio | Carregado a variavel shopping

--- src/views/checkout/index.js
+++ src/views/checkout/index.js
@@ -1,27 +1,49 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Header } from '../components/header'
+import { Link } from 'react-router-dom'
 import Custumer from './Custumer'
 import Products from './Products'
 
 export default function Checkout() {
+    const shopping = useSelector(state => state.shoppingReducer.shopping);
+
     return (
         <>
             <Header />   
 
             <div class="checkout-grid">
-                <h2>Finalizar Pedido</h2>
+                {shopping.uuid === undefined &&
+                    <>
+                        <h2>seu carrinho está vazia</h2>
 
-                <Custumer />
+                        <div style={{marginTop: '1.5rem'}}>                
+                            <div class="button-grid-card">
+                                <Link class="btn btn-checkout" to="/">
+                                    Voltar
+                                </Link>                                
+                            </div>
+                        </div>
+                    </>
+                }
 
-                <Products />
+                {shopping.uuid !== undefined &&
+                    <>
+                        <h2>Finalizar Pedido</h2>                
 
-                <div class="checkout-grid-button">                
-                    <div class="button-grid-card">
-                        <button class="btn btn-checkout">
-                            Finalizar compra
-                        </button>
-                    </div>
-                </div>
+                        <Custumer shopping={ shopping } />
+
+                        <Products shopping={ shopping } />
+
+                        <div class="checkout-grid-button">                
+                            <div class="button-grid-card">
+                                <button class="btn btn-checkout">
+                                    Finalizar compra
+                                </button>
+                            </div>
+                        </div>
+                    </>
+                }
             </div>                          
         </>
     )
