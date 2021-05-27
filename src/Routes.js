eecf5c923d1282e57513f8fa54cda264de7cf6a6
Eliel <eliel@avance.inf.br>
2021-05-27 14:24:55 -0300

    Alterado o componente routes adicionando o componente offline e a div com id online para controle do sistema offline/online

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,27 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Offline from './views/offline'
 
 const Showcase = lazy(() => import('./views/showcase'))
 const Checkout  = lazy(() => import('./views/checkout'))
 const Notfound = lazy(() => import('./views/notfound'))
 
 const Routes = () => (
-    <Router>
-        <Suspense fallback={<div>Progresso</div>}>
-            <Switch>
-                <Route exact path="/" component={Showcase} />  
-                <Route exact path="/checkout" component={Checkout} />                              
-                <Route path="*" component={Notfound} />                              
-            </Switch>
-        </Suspense>
-    </Router>
+    <>
+        <Offline />
+
+        <div id="online">  
+            <Router>
+                <Suspense fallback={<div>Progresso</div>}>
+                    <Switch>
+                        <Route exact path="/" component={Showcase} />  
+                        <Route exact path="/checkout" component={Checkout} />                              
+                        <Route path="*" component={Notfound} />                              
+                    </Switch>
+                </Suspense>
+            </Router>
+        </div>
+    </>
 )
 
 export default Routes;
