dd8d77feed288de7471df4fbad11dfcc79ad30ae
Eliel Batiston de Oliveira <batistondeoliveira@yahoo.com.br>
2021-05-26 23:51:55 -0300

    Incluido rotina para setar o timeout apos abertura do carrinho de compras

--- src/views/showcase/CustumerModal.js
+++ src/views/showcase/CustumerModal.js
@@ -2,22 +2,29 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Modal, Header, Body, Footer } from '../components/modal'
 import { uuid as uuidv4 } from 'uuidv4'
-import { openSaleAction } from '../../store/actions/cart.actions'
+import { openSaleAction, timeOutAction } from '../../store/actions/cart.actions'
 import Input from '../components/input/Input'
+import { cartOpenTimeOut } from '../../config/'
 
 export default function CustumerModal(props) {
     const dispatch = useDispatch()    
     const [ msgError, setMsgError ] = React.useState('');
     const [ custumer, setCustumer ] = React.useState('');
 
+    const timeOut = () => {
+        dispatch(timeOutAction());
+    }
+
     const addItem = () => {
         if (!custumer) {
             setMsgError('Informe seu nome para continuar a comprar :)')
 
             return ;
-        }
+        }                    
 
         dispatch(openSaleAction(uuidv4(), custumer)); 
+        setTimeout(timeOut, cartOpenTimeOut);
+
         props.onRegisterItem();                   
     }
 
