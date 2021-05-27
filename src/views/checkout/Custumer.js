382d636fbdbf86a41786845273fa84a689624dab
Eliel Batiston de Oliveira <batistondeoliveira@yahoo.com.br>
2021-05-26 23:52:23 -0300

    Alterado o componente para exibir o tempo remanescente do carrinho aberto

--- src/views/checkout/Custumer.js
+++ src/views/checkout/Custumer.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { MillisToMinutesAndSeconds } from '../components/time'
+import { cartOpenTimeOut } from '../../config'
 
-export default function Custumer(props) {
+export default function Custumer(props) { 
+    const [ timeOut, setTimeOut ] = React.useState('');
+
+    const remainingTime = () => {        
+        const now = Date.parse(new Date());        
+        const remainig = now - props.cart.createat;              
+
+        setTimeOut(MillisToMinutesAndSeconds(cartOpenTimeOut - remainig));        
+    }
+
+    React.useEffect(() => {
+        setInterval(remainingTime, 1000);
+
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);    
+
     return (                    
-        <div className="checkout-grid-custumer">
+        <div className="checkout-grid-custumer">            
             <div className="custumer-grid-card">
                 <i className="fas fa-shopping-bag"></i>
                 <div className="custumer-detail">  
                     <p className="custumer-detail-name">{ props.cart.custumer }</p>
-                    <p className="custumer-detail-timeout">09:54:34 min restante</p>
+
+                    { timeOut && <p className="custumer-detail-timeout">{ timeOut } min restante</p> }                    
                 </div>
             </div>
         </div>            
